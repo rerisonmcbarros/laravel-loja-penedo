4d7a62fcd45c80bb24fe3634ba8d4aa22c99c6f6
Rerison Barros <rerisonmcb@gmail.com>
2023-05-25 20:44:42 -0300

    feat: create sales table migration

---
+++ database/migrations/2023_05_25_233012_create_sales_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_data');
+            $table->unsignedBigInteger('total_value');
+            $table->unsignedBigInteger('discount');
+            $table->string('payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
